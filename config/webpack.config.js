707988c127a1b9f7ad66cce99b6aa099559530b6
Joon Ki Choi <joonki.choi@unifr.ch>
2017-07-29 17:57:36 +0200

    loaders break webpack-dev-server

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -17,6 +17,31 @@ var config = {
     'application': './webpack/application.js'
   },
 
+  modules: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        loader: "url?prefix=font/&limit=5000"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      },
+    ]
+  }
+
   output: {
     // Build assets directly in to public/webpack/, let webpack know
     // that all webpacked assets start with webpack/
